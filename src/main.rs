d56bf694c011a80c5ec745473f11ea4630a0b957
dchammond <dillonhammond@gmail.com>
2016-04-24 16:48:45 -0500

    Created interactive prompt for user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Rust-Calculus!");
+    println!("To evaluate an expression, simply type one in and hit RETURN.");
+    println!("To set a variable, simply type VAR_NAME=EXPRESSION and hit RETURN.");
+    println!("Valid commands are: sym_int, int, sym_def, and def.");
+    let mut input = String::new();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    loop {
+    	print!(">>>> ");
+    	stdout.flush().ok();
+        stdin.read_line(&mut input).unwrap();
+        println!("You typed: {}", input.trim());
+        input.clear();
+    }
 }
